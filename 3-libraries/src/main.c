823388b0b917964ed0e8a31723586d52345b6ca0
Luis Pina <luis@luispina.me>
2015-06-26 10:01:58 +0100

    Small fixes for OSX and removing trailing whitespaces

--- 3-libraries/src/main.c
+++ 3-libraries/src/main.c
@@ -47,18 +47,18 @@ int GZipEncode(const char* buffer, uLongf length, char** gziptext, uLongf * dest
 int Base64Encode(const char* buffer, size_t length, char** b64text) { //Encodes a binary safe base 64 string
 	BIO *bio, *b64;
 	BUF_MEM *bufferPtr;
- 
+
 	b64 = BIO_new(BIO_f_base64());
 	bio = BIO_new(BIO_s_mem());
 	bio = BIO_push(b64, bio);
- 
+
 	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Ignore newlines - write everything in one line
 	BIO_write(bio, buffer, length);
 	BIO_flush(bio);
 	BIO_get_mem_ptr(bio, &bufferPtr);
 	BIO_set_close(bio, BIO_NOCLOSE);
 	BIO_free_all(bio);
- 
+
 	*b64text=(*bufferPtr).data;
 
 	return (0); //success
@@ -67,7 +67,7 @@ int Base64Encode(const char* buffer, size_t length, char** b64text) { //Encodes 
 int main(int argc, char ** argv) {
   //Encode To Base64
   char* base64EncodeOutput;
- 
+
   Base64Encode(argv[1], (size_t) strlen(argv[1]), &base64EncodeOutput);
   printf("(base64): %s\n", base64EncodeOutput);
 
@@ -94,6 +94,6 @@ int main(int argc, char ** argv) {
   printf("(gzip): %s\n", base64);
   }
 #endif
-  
+
   return(0);
 }
